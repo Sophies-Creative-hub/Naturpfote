327d53f68f75498ae0e3cf6f24eb3a94b5e98285
Sophie <s.ischenko1990@gmail.com>
2025-12-20 21:19:45 +0100

    juhu

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/Sophies-Creative-hub/Naturpfote',
+  base: '/Naturpfote/',
   
   server: {
     port: 3000,
